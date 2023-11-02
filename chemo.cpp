8087c717b161d73cb57f432f17122ec447f09acc
Eymoji <ant67rit@gmail.com>
2023-11-02 14:39:45 +0100

    add CMake and class chemo

---
+++ chemo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ant65 on 02/11/2023.
+//
+
+#include "chemo.h"
---
+++ chemo.h
@@ -0,0 +1,14 @@
+//
+// Created by ant65 on 02/11/2023.
+//
+
+#ifndef BIOPHYSICS_PROJECT_CHEMO_H
+#define BIOPHYSICS_PROJECT_CHEMO_H
+
+
+class chemo {
+
+};
+
+
+#endif //BIOPHYSICS_PROJECT_CHEMO_H
