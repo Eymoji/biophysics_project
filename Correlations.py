b8bcdc087b726096ef302b451107b875e8d0f287
Bribrigentil <117914213+Bribrigentil@users.noreply.github.com>
2023-12-10 16:58:37 +0100

    Add files via upload

--- Correlations.py
+++ Correlations.py
@@ -24,26 +24,42 @@ number_receptor = np.shape(number_of_absorption)[1]
 
 Correlations_receptor = np.zeros((2*int(number_iteration/2)+1, number_receptor))
 
-for i in range(-int(number_iteration/2), -1):
+##for i in range(-int(number_iteration/2), 0):
+##    
+##    for j in range(number_receptor):
+##        if ((number_of_absorption[-i:,j] == 1).any()) == True:
+##            Correlations_receptor[i+int(number_iteration/2),j] = np.mean(number_of_absorption[-i:,j]*number_of_absorption[:i,j], where=(number_of_absorption[-i:,j] == 1))
+##        else:
+##            Correlations_receptor[i+int(number_iteration/2),j] = Correlations_receptor[i+int(number_iteration/2)-1,j]
+##
+##for i in range(1, int(number_iteration/2)+1):
+##    
+##    for j in range(number_receptor):
+##
+##        if ((number_of_absorption[:-i,j] == 1).any()) == True:
+##            Correlations_receptor[i+int(number_iteration/2),j] = np.mean(number_of_absorption[:-i,j]*number_of_absorption[i:,j]), where=(number_of_absorption[:-i,j] == 1))
+##        else:
+##            Correlations_receptor[i+int(number_iteration/2),j] = Correlations_receptor[i+int(number_iteration/2)-1,j]
+##
+##for j in range(number_receptor):
+##    
+##    Correlations_receptor[int(number_iteration/2),j] = 1
 
+for i in range(-int(number_iteration/2), 0):
+    
     for j in range(number_receptor):
-        if ((number_of_absorption[-i:,j] == 1).any()) == True:
-            Correlations_receptor[i+int(number_iteration/2),j] = np.mean(number_of_absorption[-i:,j]*number_of_absorption[:i,j], where=(number_of_absorption[-i:,j] == 1))
-        else:
-            Correlations_receptor[i+int(number_iteration/2),j] = Correlations_receptor[i+int(number_iteration/2)-1,j]
-          
-for i in range(1, int(number_iteration/2)):
 
+        Correlations_receptor[i+int(number_iteration/2),j] = np.mean(number_of_absorption[-i:,j]*number_of_absorption[:i,j])
+
+for i in range(1, int(number_iteration/2)+1):
+    
     for j in range(number_receptor):
 
-        if ((number_of_absorption[:-i,j] == 1).any()) == True:
-            Correlations_receptor[i+int(number_iteration/2),j] = np.mean(number_of_absorption[:-i,j]*number_of_absorption[i:,j], where=(number_of_absorption[:-i,j] == 1))
-        else:
-            Correlations_receptor[i+int(number_iteration/2),j] = Correlations_receptor[i+int(number_iteration/2)-1,j]
+        Correlations_receptor[i+int(number_iteration/2),j] = np.mean(number_of_absorption[:-i,j]*number_of_absorption[i:,j])
 
 for j in range(number_receptor):
     
-    Correlations_receptor[int(number_iteration/2),j] = 1
+    Correlations_receptor[int(number_iteration/2),j] = (Correlations_receptor[int(number_iteration/2)-1,j]+Correlations_receptor[int(number_iteration/2+1),j])/2
 
 
 plt.figure(figsize=(16,16))
@@ -62,22 +78,16 @@ plt.xlabel("tau (s)")
 plt.ylabel("Correlation")
 plt.show()
 
-for j in range(number_receptor):
-    print(Correlations_receptor[int(number_iteration/2)-1,j])
-print(Averaged_Correlations[int(number_iteration/2)-1])
-
-
 
 def fit_correlations(tau, p_moy, tau_b):
     return p_moy**2 + p_moy*(1-p_moy)*np.exp(-abs(tau)/((1-p_moy)*tau_b))
 
-popt, pcov = curve_fit(fit_correlations, Tau, Averaged_Correlations, p0 = np.array([0.5, 5]))
+popt, pcov = curve_fit(fit_correlations, Tau, Averaged_Correlations, p0 = np.array([0.2, 5]))
 print(popt)
-
 
 plt.figure(figsize=(16,16))
 plt.plot(Tau, Averaged_Correlations)
-plt.plot(Tau, fit_correlations(popt[0], popt[1], Tau))
+plt.plot(Tau, fit_correlations(Tau, popt[0], popt[1]))
 plt.xlabel("tau (s)")
 plt.ylabel("Correlation")
 plt.show()
